a5e3d7b1f98b2745abcbb4acf8791340bf95c709
Baris Demiray <baris.demiray@gmail.com>
2018-11-10 22:36:21 +0100

    T241 Improve disk usage integration

--- disk/check_disk_usage.sh
+++ disk/check_disk_usage.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 # A sample script that notifies Statuscope.io of available disk space in /home
-# Example cron entry: 0 * * * * cd /opt/scripts/ && ./check_disk_space.sh
+# Example cron entry: 0 * * * * /opt/statuscope.io/disk/check_disk_space.sh <token> <task_id> /home 90
 
-disk_usage=$(df --output=pcent /home | tr -dc '0-9')
+# Verify parameters
+if [ "$#" -ne 4 ]; then
+    echo "Tip: ./check_disk_space.sh <token> <jobId> <partition> <usageWarningThreshold>"
+    echo "Example: ./check_website.sh 1ba04f74 HwtnPK5JZSHkYzFpA /home 90"
+    exit 1
+fi
 
-if [[ disk_usage -gt 90 ]]; then
-    echo "Disk usage of /home has passed 90%"
+TOKEN=$1
+TASK_ID=$2
+PARTITION=$3
+THRESHOLD=$4
+
+echo "token=${TOKEN} task_id=${TASK_ID} part=${PARTITION} threshold=${THRESHOLD}"
+
+disk_usage=$(df --output=pcent ${PARTITION} | tr -dc '0-9')
+
+if [[ disk_usage -gt $THRESHOLD ]]; then
+    echo "Disk usage of ${PARTITION} has passed ${THRESHOLD}%"
     curl -H "Content-Type: application/json" \
          -X POST \
-         -d '{"token":"95ef3bf7", "status":"KO", "reason": "Disk usage of /home is over 90%"}' \
-         https://www.statuscope.io/tasks/qMcQax5AczvAfwcXW
+         -d '{"token":"'"${TOKEN}"'", "status":"KO", "reason": "'"Disk usage of ${PARTITION} is over ${THRESHOLD}%"'"}' \
+         https://api.statuscope.io/tasks/${TASK_ID}
 else
-    echo "Disk usage of /home is less than 90%"
+    echo "Disk usage of ${PARTITION} is less than ${THRESHOLD}%"
     curl -H "Content-Type: application/json" \
          -X POST \
-         -d '{"token":"95ef3bf7", "status":"OK", "reason": "'"/home disk usage is at ${disk_usage}%"'"}' \
-         https://www.statuscope.io/tasks/qMcQax5AczvAfwcXW
+         -d '{"token":"'"${TOKEN}"'", "status":"OK", "reason": "'"${PARTITION} disk usage is at ${disk_usage}%"'"}' \
+         https://api.statuscope.io/tasks/${TASK_ID}
 fi
 
