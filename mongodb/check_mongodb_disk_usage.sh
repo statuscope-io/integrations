eabe3924a7944f9baa7e540f19a9219b6f057742
Baris Demiray <baris.demiray@gmail.com>
2019-02-04 18:35:59 +0100

    T304 BUG Fix a copy/paste error in the example command

--- mongodb/check_mongodb_disk_usage.sh
+++ mongodb/check_mongodb_disk_usage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# A sample script that notifies Statuscope.io of used disk space by a mongodb server
+# A sample script that notifies Statuscope.io of used disk space of a mongodb server
 # Example cron entry: 0 * * * * /opt/statuscope.io/disk/check_mongodb_disk_usage.sh <mongoURL> <mongoUser> <mongoPwd> <token> <taskId> <thresholdInBytes>
 # Note that this script expects password in cleartext so do not add it to crontasks of a shared account
 
 # Verify parameters
 if [ "$#" -ne 6 ]; then
     echo "Tip: ./check_mongodb_disk_usage.sh <mongoURL> <mongoUser> <mongoPwd> <token> <taskId> <thresholdInMegabytes>"
-    echo "Example: ./check_website.sh 10.10.0.0:27017/testdb testuser sknEzEc8apLP41Zg 1ba012f74 HwPK5JZSHkYzFpA 1024"
+    echo "Example: ./check_mongodb_disk_usage.sh 10.10.0.0:27017/testdb testuser sknEzEc8apLP41Zg 1ba012f74 HwPK5JZSHkYzFpA 1024"
     exit 1
 fi
 
